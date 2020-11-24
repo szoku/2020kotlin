c7d8ea395da44bc3085fda59d223776bc3286fa7
Piotr Szok <piotr.szok@gmail.com>
2020-11-24 07:15:56 +0100

    Equality

--- HelloWorld/src/academy/declarations/Declarations.kt
+++ HelloWorld/src/academy/declarations/Declarations.kt
@@ -3,6 +3,24 @@ package academy.declarations
 typealias EmployeeSet = Set<Employee>
 
 fun main(args: Array<String>) {
+
+    val employeeOne = Employee("Mary", 1)
+    val employeeTwo = Employee("John", 2)
+    val employeeThree = Employee("John", 2)
+    println(employeeOne == employeeTwo)
+    println(employeeTwo == employeeThree)
+    println(employeeOne === employeeTwo)
+    println(employeeTwo === employeeThree)
+    println(employeeOne.equals(employeeTwo))
+    println(employeeThree.equals(employeeTwo))
+    val employeeFour = employeeTwo
+    println(employeeTwo === employeeFour)
+
+    println(employeeTwo != employeeFour)
+    println(employeeTwo !== employeeFour)
+    println(employeeTwo != employeeThree)
+    println(employeeTwo !== employeeThree)
+
     var number: Int
     number = 10
     number = 20
@@ -23,5 +41,13 @@ fun main(args: Array<String>) {
 }
 
 class Employee(var name: String, val id: Int) {
+
+    override fun equals(obj: Any?): Boolean {
+        if (obj is Employee) {
+            return name == obj.name && id == obj.id
+        }
+
+        return false
+    }
 
 }
