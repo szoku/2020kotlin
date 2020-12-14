6201ca0986779c54a88a37ad4bb447725de02279
Piotr Szok <piotr.szok@gmail.com>
2020-12-14 07:20:54 +0100

    Inheritance 1

---
+++ IInheritance/src/academy/inheritance/Printer.kt
@@ -0,0 +1,18 @@
+package academy.inheritance
+
+fun main(args: Array<String>) {
+    val laserPrinter = LaserPrinter("Brother 1234", 15)
+    laserPrinter.printModel()
+}
+
+abstract class Printer(val modelName: String) {
+
+    open fun printModel() = println("The model name of this printer is $modelName")
+    abstract fun bestSellingPrice(): Double
+}
+
+open class LaserPrinter(modelName: String, pagePerMinute: Int): Printer(modelName) {
+
+    final override fun printModel() = println("The model name of this laser printer is $modelName")
+    override fun bestSellingPrice(): Double = 129.99
+}
