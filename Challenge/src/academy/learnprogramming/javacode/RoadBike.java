98d97923c5b5352b8e5665fbd85c61ece58e8429
Piotr Szok <piotr.szok@gmail.com>
2021-01-12 09:16:37 +0100

    Challenge section 5

---
+++ Challenge/src/academy/learnprogramming/javacode/RoadBike.java
@@ -0,0 +1,20 @@
+package academy.learnprogramming.javacode;
+
+public class RoadBike extends Bicycle {
+
+    // In millimetres
+    private int tireWidth;
+
+    public RoadBike(int cadence,
+                    int speed,
+                    int gear,
+                    int tireWidth) {
+        super(cadence, speed, gear);
+        this.tireWidth = tireWidth;
+    }
+
+    public int getTireWidth() {
+        return tireWidth;
+    }
+
+}
