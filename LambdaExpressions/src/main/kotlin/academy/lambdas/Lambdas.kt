fbd9ade783b0d4a03de00c32f317ddf8fcef4eae
Piotr Szok <piotr.szok@gmail.com>
2021-02-15 08:09:11 +0100

    Apply()

--- LambdaExpressions/src/main/kotlin/academy/lambdas/Lambdas.kt
+++ LambdaExpressions/src/main/kotlin/academy/lambdas/Lambdas.kt
@@ -4,20 +4,37 @@ import java.lang.StringBuilder
 
 fun main(args: Array<String>) {
 
-    println(countTo100())
+    println(countTo100Apply())
 
-    val eployees = listOf(
+    val employees = listOf(
         Employee("John", "Smith", 2012),
         Employee("Jane", "Wilson", 2015),
         Employee("Mary", "Johnson", 2010),
         Employee("Mike", "Jones", 2002)
     )
 
+    findByLastName(employees, "Wilson")
+
+    "Some String".apply {
+        "Another String".apply {
+            println(toLowerCase())
+        }
+    }
+
 }
 
-fun countTo100(): String {
-    val numbers = StringBuilder()
-    return with(numbers) {
+fun findByLastName(employees: List<Employee>, lastName: String) {
+    employees.forEach() {
+        if (it.lastName == lastName) {
+            println("Yes, an employee with last name $lastName")
+            return
+        }
+    }
+    println("No such a last name $lastName")
+}
+
+fun countTo100With() =
+    with(StringBuilder()) {
         for (i in 1..99) {
             append(i)
             append(", ")
@@ -25,7 +42,15 @@ fun countTo100(): String {
         append(100)
         toString()
     }
-}
+
+fun countTo100Apply() =
+    StringBuilder().apply() {
+        for (i in 1..99) {
+            append(i)
+            append(", ")
+        }
+        append(100)
+    }.toString()
 
 /*fun countTo100(): String {
     val numbers = StringBuilder()
